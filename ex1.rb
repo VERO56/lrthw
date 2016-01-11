4b82376ecb727ef462bdbb994433afd7ea4b1809
jbpasquier <contact@jbpasquier.eu>
2016-01-11 16:52:09 +0100

    tous mes exo

---
+++ ex1.rb
@@ -0,0 +1,8 @@
+puts "Hello Worlds!"
+puts "Hello Again"
+puts "I like typing this"
+puts "This is fun"
+puts "Yay! Printing."
+puts "I'd must rather you 'not'"
+puts 'I "SAID" DO NOT TOUCH THIS'
+
---
+++ ex2.rb
@@ -0,0 +1,9 @@
+# A comment, this is sor you can read your program later.
+# Anything after the # is ignored by ruby.
+
+puts "I could have code like this" #and the comment after is ignore
+
+# You can also use a comment to "disable" or comment out a piece of code.
+# Puts "This won't run."
+
+puts "This will run"
